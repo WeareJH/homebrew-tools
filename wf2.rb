af3a1b3919dd5d9339b5b15f1f1a39e72c515f6d
Michael Woodward <mikeymike.mw@gmail.com>
2019-06-10 22:02:10 +0100

    wf2 through homebrew, maybe

---
+++ wf2.rb
@@ -0,0 +1,11 @@
+class Wf2 < Formula
+  homepage 'https://github.com/WeareJH/wf2'
+
+  url 'https://github.com/WeareJH/wf2/releases/latest/download/wf2'
+  version '0.7.2'
+  sha256 'a1caac045d886d4cbd4711895f05d4e082f3e449b568be1f1732674e318c2e9a'
+
+  def install
+    bin.install 'wf2'
+  end
+end
