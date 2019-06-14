f644a12436f7ddddf7034cd85a9228f0ad214317
Diego Cabrejas <diego@wearejh.com>
2019-06-14 17:38:21 +0100

    use latest binary

--- wf2.rb
+++ wf2.rb
@@ -1,7 +1,7 @@
 class Wf2 < Formula
   homepage 'https://github.com/WeareJH/wf2'
 
-  url 'https://github.com/WeareJH/wf2/releases/download/v0.7.2/wf2'
+  url 'https://github.com/WeareJH/wf2/releases/latest/download/wf2'
   version '0.7.2'
   sha256 '762aa272fdb3ab8103b25c2a2ccbfd6e3e3f59aa4af41683f09f44c36a1b6f83'
 
